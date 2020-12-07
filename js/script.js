3648e8430786d017929e939a52f6c9a391a42d09
E Ewusi-Essel <ewusiessel@gmail.com>
2020-12-07 01:41:44 +0000

    gallery section placed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// owl carousel
+
 $(".owl-carousel").owlCarousel({
     margin:10,
     responsiveClass: true,
@@ -15,3 +17,17 @@ $(".owl-carousel").owlCarousel({
        },  
     }
 });
+
+
+// google maps
+
+    function initMap() {
+        let uluru = {lat: 51.509865, lng: -0.118092};
+        
+        let map = new google.maps.Map(
+            document.getElementById('map'), {zoom: 13, center:uluru}
+        );
+        let marker = new google.maps.Marker({position: uluru, map: map})
+        
+    }
+
